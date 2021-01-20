3ffa6608d47cbcda8be65a43dd47bf381e6b7ae3
Diogo André Romão <diogo.romao@visma.com>
2021-01-20 08:33:03 +0000

    Uncomment commented code

--- src/LinkfireTechChallenge/Startup.cs
+++ src/LinkfireTechChallenge/Startup.cs
@@ -116,10 +116,10 @@ namespace LinkfireTechChallenge
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            //if (env.IsDevelopment())
-            //{
-            //    app.UseDeveloperExceptionPage();
-            //}
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
 
             app.UseRouting();
 
